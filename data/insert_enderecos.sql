a5a3d130224f9e5997ff362622a99a027cb3e136
Luiz Eduardo Lima Balbino <luizeduardo071205@gmail.com>
2025-06-02 20:46:15 -0300

    update endereco insertion script with new address data and correct column order

--- data/insert_enderecos.sql
+++ data/insert_enderecos.sql
@@ -1,12 +1,8 @@
-INSERT INTO Endereco (cep, logradouro, numero, complemento, bairro, cidade, uf)
+INSERT INTO Endereco ( logradouro, numero, complemento, bairro, cidade, uf, cep)
 VALUES
-('12345-678', 'Rua A', 100, 'Apto 1', 'Bairro A', 'Cidade A', 'AB'),
-('23456-789', 'Rua B', 200, 'Apto 2', 'Bairro B', 'Cidade B', 'BC'),
-('34567-890', 'Rua C', 300, 'Apto 3', 'Bairro C', 'Cidade C', 'CD'),
-('45678-901', 'Rua D', 400, 'Apto 4', 'Bairro D', 'Cidade D', 'DE'),
-('56789-012', 'Rua E', 500, 'Apto 5', 'Bairro E', 'Cidade E', 'EF'),
-('67890-123', 'Rua F', 600, 'Apto 6', 'Bairro F', 'Cidade F', 'FG'),
-('78901-234', 'Rua G', 700, 'Apto 7', 'Bairro G', 'Cidade G', 'GH'),
-('89012-345', 'Rua H', 800, 'Apto 8', 'Bairro H', 'Cidade H', 'HI'),
-('90123-456', 'Rua I', 900, 'Apto 9', 'Bairro I', 'Cidade I', 'IJ'),
-('01234-567', 'Rua J', 1000, 'Apto 10', 'Bairro J', 'Cidade J', 'JK');
+('Rua das Flores', '123', 'Apto 45', 'Jardim das Rosas', 'São Paulo', 'SP', '01234-567'),
+('Avenida Paulista', '456', '', 'Bela Vista', 'São Paulo', 'SP', '01310-000'),
+('Rua da Liberdade', '789', '', 'Liberdade', 'São Paulo', 'SP', '01502-000'),
+('Avenida Brasil', '1011', 'Sala 202', 'Centro', 'Rio de Janeiro', 'RJ', '20000-000'),
+('Rua das Acácias', '1213', '', 'Jardim Botânico', 'Rio de Janeiro', 'RJ', '22260-000'),
+('Praça da Sé', '', '', 'Sé', 'São Paulo', 'SP', '01001-000');
